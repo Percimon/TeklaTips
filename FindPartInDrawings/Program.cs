b18c330926e671b7df662ddc9696d701d41ff618
Vladislav Lakeev <chilli.willi.930@gmail.com>
2022-06-05 20:27:01 +0300

    sheet searcher added

--- FindPartInDrawings/Program.cs
+++ FindPartInDrawings/Program.cs
@@ -13,6 +13,8 @@ namespace FindPartInDrawings
         static void Main(string[] args)
         {
             ViewSearcher();
+            Console.WriteLine("=== results should be the same ===");
+            SheetSearcher();
         }
 
         private static void ViewSearcher()
@@ -59,6 +61,54 @@ namespace FindPartInDrawings
                         }
                         selectedObjects.Reset();
                     }
+                }
+                Console.WriteLine("done");
+            }
+            else
+            {
+                Console.WriteLine("No connection");
+            }
+        }
+
+        private static void SheetSearcher()
+        {
+            ModelObjectEnumerator selectedObjects = new Tekla.Structures.Model.UI.ModelObjectSelector().GetSelectedObjects();
+            if (selectedObjects.GetSize() == 0)
+            {
+                Console.WriteLine("no object selected");
+                return;
+            }
+
+            DrawingHandler drawingHandler = new DrawingHandler();
+            if (drawingHandler.GetConnectionStatus())
+            {
+                DrawingEnumerator selectedDrawings = drawingHandler.GetDrawingSelector().GetSelected();
+                if (selectedDrawings.GetSize() == 0)
+                {
+                    Console.WriteLine("no drawing selected");
+                    return;
+                }
+
+                while (selectedDrawings.MoveNext())
+                {
+                    Drawing currentDrawing = selectedDrawings.Current;
+                    ContainerView sheet = currentDrawing.GetSheet();
+                    while (selectedObjects.MoveNext())
+                    {
+                        if (selectedObjects.Current is Tekla.Structures.Model.Part part)
+                        {
+                            if (sheet.GetModelObjects(part.Identifier).GetSize() > 0)
+                            {
+                                Console.WriteLine($"{part.Identifier.GUID} was found in " +
+                                    $"drawing: {currentDrawing.Name}");
+                            }
+                            else
+                            {
+                                Console.WriteLine($"{part.Identifier.GUID} not found");
+                            }
+                        }
+                    }
+                    selectedObjects.Reset();
                 }
                 Console.WriteLine("done");
             }
